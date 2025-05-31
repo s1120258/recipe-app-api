e46b1349647592c7b4b6ee6375b2d32c30016847
MK <m.kanamaru.it@gmail.com>
2025-05-31 10:38:20 -0400

    add sample unit tests

---
+++ app/app/calc.py
@@ -0,0 +1,21 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """
+    Add two numbers together.
+    :param x: First number
+    :param y: Second number
+    :return: Sum of x and y
+    """
+    return x + y
+  
+def subtract(x, y):
+    """
+    Subtract second number from first number.
+    :param x: First number
+    :param y: Second number
+    :return: Result of x - y
+    """
+    return x - y
